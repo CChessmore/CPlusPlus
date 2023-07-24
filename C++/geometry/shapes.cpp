2599dbecac315c72b603a122ebd837ffd41c5dd9
Conner Chessmore <connerchessmore@gmail.com>
2023-07-24 12:02:30 -0500

    Added constructors to shape classes

--- C++/geometry/shapes.cpp
+++ C++/geometry/shapes.cpp
@@ -1,34 +1,90 @@
 //Declare classes for square, triangle, circle for 2D shapes
 class Square {
+    public:
+        int sides = 4;
+        float side_length;
+        Square::createSquare(float size) {
+            this->side_length = size;
+        }
 
 };
 
-class triangle {
+class Triangle {
+    public:
+        int sides = 3;
+        float side1;
+        float side2;
+        float side3;
+        Triangle::createTriangle(float sideA, float sideB, float sideC) {
+            this->side1 = sideA;
+            this->side2 = sideB;
+            this->side3 = sideC;
+        }
 
 };
 
-class circle {
+class Circle {
+    public:
+        float radius;
+        Circle::createCircle(float size) {
+            this->radius = size;
+        }
 
 };
 
 //Declare classes for cube, 3 & 4 sided pyramids, cylinder, and sphere for 3D shapes
 
 class Cube {
+    public:
+        int faces = 6;
+        float height;
+        float width;
+        float depth;
+        Cube::makeCube(float h, float w, float d) {
+            this->height = h;
+            this->width = w;
+            this->depth = d;
+        }
     
 };
 
+//The pyramids will assume equal side lengths and only ask once for base size
+//and once for the height of the object.
 class PyramidBase3 {
+    public:
+        float side_lengths;
+        float height;
+        PyramidBase3::createBaseThree(float l, float h) {
+            this->side_lengths = l;
+            this->height = h;
+        }
 
 };
 
 class PyramidBase4 {
-
+    public:
+        float side_lengths;
+        float height;
+        PyramidBase4::createBaseFour(float l, float h) {
+            this->side_lengths = l;
+            this->height = h;
+        }
 };
 
 class Cylinder {
+    public:
+        float radius;
+        float height;
+        Cylinder::createCylinder(float r, float h) {
+            this->radius = r;
+            this->height = h;
+        }
 
 };
 
 class Sphere {
-    
+    float radius;
+    Sphere::createSphere(float r) {
+        this->radius = r;
+    }
 };
