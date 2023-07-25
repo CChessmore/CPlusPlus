422d5de88848401446cf302f58fbc512334bb379
Conner Chessmore <connerchessmore@gmail.com>
2023-07-25 15:22:37 -0500

    Rename folder; fix constructors

--- Basics/geometry/shapes.cpp
+++ Basics/geometry/shapes.cpp
@@ -1,12 +1,10 @@
-//PLEASE NOTE
-//All constructors and functions assume valid input!
-
 //Declare classes for square, triangle, circle for 2D shapes
 class Square {
     public:
         int sides = 4;
         float side_length;
-        Square::createSquare(float size) {
+
+        Square(float size) {
             this->side_length = size;
         }
 
@@ -14,21 +12,25 @@ class Square {
 
 class Triangle {
     public:
-        float sideA, sideB, sideC;
-        //Constructor takes 3 float inputs. It's possible to create triangles that
-        //aren't "real" with this current implementation.
-        Triangle::newTriangle(float side1, float side2, float side3){
-            this->sideA = side1;
-            this->sideB = side2;
-            this->sideC = side3;
+        int sides = 3;
+        float side1;
+        float side2;
+        float side3;
+
+        Triangle(float sideA, float sideB, float sideC) {
+            this->side1 = sideA;
+            this->side2 = sideB;
+            this->side3 = sideC;
         }
+
 };
 
 class Circle {
     public:
         float radius;
-        Circle::createCircle(float r) {
-            this->radius = r;
+
+        Circle(float size) {
+            this->radius = size;
         }
 
 };
@@ -36,12 +38,13 @@ class Circle {
 //Declare classes for cube, 3 & 4 sided pyramids, cylinder, and sphere for 3D shapes
 
 class Cube {
-    //Technically, this class can handle rectangular prisms as well as cubes
     public:
+        int faces = 6;
         float height;
         float width;
         float depth;
-        Cube::makeCube(float h, float w, float d) {
+
+        Cube(float h, float w, float d) {
             this->height = h;
             this->width = w;
             this->depth = d;
@@ -53,10 +56,10 @@ class Cube {
 //and once for the height of the object.
 class PyramidBase3 {
     public:
-    //Base3 is traingular base pyramids
         float side_lengths;
         float height;
-        PyramidBase3::createBaseThree(float l, float h) {
+
+        PyramidBase3(float l, float h) {
             this->side_lengths = l;
             this->height = h;
         }
@@ -65,21 +68,21 @@ class PyramidBase3 {
 
 class PyramidBase4 {
     public:
-    //Base4 pyramids are square on bottom, like the pyramids in Egypt
         float side_lengths;
         float height;
-        PyramidBase4::createBaseFour(float l, float h) {
+
+        PyramidBase4(float l, float h) {
             this->side_lengths = l;
             this->height = h;
         }
 };
 
 class Cylinder {
-    //Possible future project to handle calculus operations with this class?
     public:
         float radius;
         float height;
-        Cylinder::createCylinder(float r, float h) {
+
+        Cylinder(float r, float h) {
             this->radius = r;
             this->height = h;
         }
@@ -87,10 +90,9 @@ class Cylinder {
 };
 
 class Sphere {
-    //It's spherical!
-    public:
-        float radius;
-        Sphere::createSphere(float r) {
-            this->radius = r;
-        }
+    float radius;
+    
+    Sphere(float r) {
+        this->radius = r;
+    }
 };
