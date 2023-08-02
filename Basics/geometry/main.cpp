d59d346468bef80591e0bc36a3fd99b27bc08a3e
Conner Chessmore <connerchessmore@gmail.com>
2023-08-01 20:22:30 -0500

    Fixed typo

--- Basics/geometry/main.cpp
+++ Basics/geometry/main.cpp
@@ -1,7 +1,7 @@
 #include "Geometry.hh"
 
 //Declare some 2d shapes with random values, perform some math on them
-//This is done with objects/classes and self-coded functios, not libraries
+//This is done with objects/classes and self-coded functions, not libraries
 
 int main(){
     return 0;
