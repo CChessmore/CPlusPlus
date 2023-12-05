a46205eaa747eed67f381a78ee0a7dda18a6069b
CChessmore <connerchessmore@gmail.com>
2023-12-05 12:51:52 -0600

    Added == operator overloads to 2d shapes

--- Basics/geometry/shapes.cpp
+++ Basics/geometry/shapes.cpp
@@ -1,5 +1,6 @@
 //Get some more math functionality access
 #include <math.h>
+#include <algorithm>
 
 //Declare classes for square, triangle, circle for 2D shapes
 class Square {
@@ -19,6 +20,12 @@ class Square {
             return this->side_length*4;
         }
 
+    //Compare side_length of squares. If these are not equal, return false, otherwise it will return as True
+    friend auto operator ==(Square s1, Square s2)
+    {
+        return (s1.side_length == s2.side_length);
+        
+    }
 };
 
 class Triangle {
@@ -46,6 +53,22 @@ class Triangle {
             return this->side1+this->side2+this->side3;
         }
 
+        //Put 3 sides in each array, sort, and compare. If triangles are equal, these will match and return True
+        friend auto operator ==(Triangle t1, Triangle t2)
+        {
+            float t1_arr[] = {t1.side1, t1.side2, t1.side3};
+            float t2_arr[] = {t2.side1, t2.side2, t2.side3};
+
+            sort(t1_arr,3);
+            sort(t2_arr,3);
+
+            for(int i=0; i<3; i++)
+            {
+                if(t1_arr[i]!=t2_arr[i]) { return false; }
+            }
+            return true;
+        }
+
 };
 
 class Circle {
@@ -63,7 +86,12 @@ class Circle {
         float get_perimeter() {
             return 2*M_PI*this->radius;
         }
-
+        
+        //If radii are the same, the circles are equal (in this implementation. I don't know if there's wild magic math to disprove this)
+        friend auto operator ==(Circle c1, Circle c2)
+        {
+            return (c1->radius == c2->radius);
+        }
 };
 
 //Declare classes for cube, 3 & 4 sided pyramids, cylinder, and sphere for 3D shapes
